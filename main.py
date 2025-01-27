69ff578d54509ef95b02cc3246e1195bd12c4767
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2025-01-28 09:21:58 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from functions.py import FilteredArticles
+
+# RSS feed URLs
+world_news = [
+    'https://feeds.bbci.co.uk/news/business/rss.xml?edition=uk',
+    'https://feeds.bbci.co.uk/news/education/rss.xml?edition=uk',
+    'https://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml?edition=uk',
+    'https://feeds.bbci.co.uk/news/health/rss.xml?edition=uk',
+    'https://feeds.bbci.co.uk/news/technology/rss.xml?edition=uk',
+    'https://feeds.bbci.co.uk/news/world/rss.xml?edition=uk',
+    'https://feeds.bbci.co.uk/news/science_and_environment/rss.xml?edition=uk'
+]
+
+# Create the main class instance
+filtered_articles = FilteredArticles(world_news)
+
+# Fetch RSS articles
+df_articles = filtered_articles.fetch_rss_articles()
+
+# Fetch webpage metadata
+df_metadata = filtered_articles.fetch_webpage_metadata()
+
+# Filter articles published today
+df_filtered = filtered_articles.filter_by_date()
